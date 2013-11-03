39e1d1750f7d319cfa17a34eea132f7ac5add5d4
Mashpy <mashpy@mashpy-HP-G42-Notebook-PC.(none)>
2013-11-03 12:03:42 +0600

    test successful for job_posts table and also for title and description attribute.

--- app/controllers/job_posts_controller.rb
+++ app/controllers/job_posts_controller.rb
@@ -1,4 +1,24 @@
 class JobPostsController < ApplicationController
   def index
   end
+
+  def new
+  @title="Add Jobs"
+  end
+
+  def create
+  @job_post = JobPosts.new(job_posts_params)
+  @job_post.save
+  redirect_to @job_post
+  end
+    
+  def show
+  @job_post = JobPosts.find(params[:id])
+  end
+
+  private
+  def job_posts_params
+  params.require(:job_posts).permit(:title, :description)
+  end
+  
 end

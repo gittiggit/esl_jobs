1af34b0dc59fd9ead149a57ac415edf2dda7c956
Mashpy <mashpy@mashpy-HP-G42-Notebook-PC.(none)>
2013-11-06 16:59:57 +0600

    added edit page and you can update jobs.

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,18 +1,42 @@
 class CountriesController < ApplicationController
   def index
+     @countries= Country.all
   end
   def new
   @title="Add Jobs"
+  @country = Country.new
   end
 
   def create
   @country = Country.new(country_params)
-  @country.save
+  if @country.save
   redirect_to @country
+  else
+    render 'new'
+  end
   end
     
   def show
   @country = Country.find(params[:id])
+  end
+  
+  def edit
+  @country = Country.find(params[:id])
+  end
+  
+  def update
+  @country = Country.find(params[:id])
+  if @country.update(country_params)
+    redirect_to @country
+  else
+    render 'edit'
+  end
+  end
+
+  def destroy
+  @country = Country.find(params[:id])
+  @country.destroy
+  redirect_to countries_path
   end
 
   private
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,3 +1,5 @@
 class Country < ActiveRecord::Base
   has_many :job_posts
+   validates :countryname, presence: true,
+                    length: { minimum: 3 }
 end

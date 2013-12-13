2bd2279df6b65d13cebab951999e38f05d3b8498
Mashpy <mashpy@mashpy-HP-G42-Notebook-PC.(none)>
2013-12-13 20:29:43 +0600

    slug will be created according to title. And i limit the titile length.

--- app/models/job_post.rb
+++ app/models/job_post.rb
@@ -1,8 +1,12 @@
 class JobPost < ActiveRecord::Base
   extend FriendlyId
-  friendly_id :id, use: :history
+  friendly_id :title, use: :history
   def should_generate_new_friendly_id?
     new_record?
+  end
+  
+  def normalize_friendly_id(string)
+  super[0..80]
   end
 
   belongs_to :country

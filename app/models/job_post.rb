01b13c8c9c7e5edf1ac452d0800fe523963aaed0
Mashpy <mashpy@mashpy-desktop.(none)>
2013-12-18 21:10:46 +0600

    multiple attribute added in search method

--- app/models/job_post.rb
+++ app/models/job_post.rb
@@ -15,7 +15,7 @@ class JobPost < ActiveRecord::Base
 #  validates_length_of :description, :maximum => 5                  
 
   def self.search(search)
-  where('title LIKE ?', "%#{search}%")
+  where('title LIKE? OR description LIKE?', "%#{search}%","%#{search}%")
   end
 
 end

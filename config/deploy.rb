40f82850b07f1353440fa34e4d1cf3fafa478642
Mashpy <mashpy@mashpy-desktop.(none)>
2013-12-30 18:52:09 +0600

    added files for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,53 @@
+set :stages, %w[staging production]
+set :default_stage, 'staging'
+require "bundler/capistrano"
+
+server "162.243.21.171", :web, :app, :db, primary: true
+
 set :application, "esl_jobs"
-set :repository, "set your repository location here"
+set :user, "mashpy"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :scm, "git"
+set :repository, "git@github.com:mashpy/#{application}.git"
+set :branch, "master"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db, "your db-server here", :primary => true
-
-after "deploy:symlink", "deploy:update_crontab"
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
 namespace :deploy do
-  desc "Update the crontab file"
-  task :update_crontab, :roles => :db do
-    run "cd #{release_path} && whenever --update-crontab #{application}"
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
   end
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
 end

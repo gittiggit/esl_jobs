a9d9bc2ad24bd6b8ec6cddca9299f0314affc04a
Mashpy <mashpy@mashpy-desktop.(none)>
2013-12-30 20:44:51 +0600

    deploy testing

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:Mashpy/#{application}.git"
+set :repository, "https://github.com/Mashpy/esl_jobs.git"
 set :branch, "master"
 
 default_run_options[:pty] = true

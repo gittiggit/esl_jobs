b3ba542921e27c3e1ce83a8aae5e129c84a8ede9
Mashpy <mashpy@mashpy-desktop.(none)>
2014-01-04 00:43:40 +0600

    update alphatest to master

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :use_sudo, false
 
 set :scm, "git"
 set :repository, "https://github.com/Mashpy/esl_jobs.git"
-set :branch, "alphatest"
+set :branch, "master"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true

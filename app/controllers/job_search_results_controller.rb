1d2dccaa65b5da90ecc724c398a5f5647a2396b7
Mashpy Says <likewarid@gmail.com>
2016-01-02 15:07:02 +0000

    country search is not right position

--- app/controllers/job_search_results_controller.rb
+++ app/controllers/job_search_results_controller.rb
@@ -1,5 +1,6 @@
 class JobSearchResultsController < ApplicationController
   def index
     @job_posts = JobPost.advancesearch(params[:keyword],params[:countrysearch]).paginate(:page => params[:page], :per_page => 39,:order => "updated_at DESC" , total_entries: 100)
+    @countries = Country.all
   end
 end

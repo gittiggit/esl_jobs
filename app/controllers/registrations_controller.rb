90770f6879e3299d1ea1b4bca449271e3d604451
Mashpy Says <likewarid@gmail.com>
2016-01-08 20:54:05 +0000

    username will taken from first and second name

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,13 @@ class RegistrationsController < Devise::RegistrationsController
  
 private
   def sign_up_params
-    params.require(:user).permit( :email, :username, :first_name, :last_name, :citizenship , :businessname, :city, :country, 	:address, :about, :telephone, :website, :otherlocation, :birthdate, :introduction, :employees, 	:locationmap, :locationdescription, :skills, :language, :avatar, :avatar_cache, :password, :password_confirmation, :current_password, :role_type_id)
+    if params[:user][:role_type_id] == 1
+      new_username = params[:user][:first_name].split.join.to_s + params[:user][:last_name].split.join.to_s + (5...30).sort_by{rand}[2].to_s
+    else
+      new_username = params[:user][:businessname].split.join.to_s + (5...30).sort_by{rand}[2].to_s
+    end  
+
+    params.require(:user).permit( :email, :username, :first_name, :last_name, :citizenship , :businessname, :city, :country, 	:address, :about, :telephone, :website, :otherlocation, :birthdate, :introduction, :employees, 	:locationmap, :locationdescription, :skills, :language, :avatar, :avatar_cache, :password, :password_confirmation, :current_password, :role_type_id).merge(:username => new_username)
   end
 
   def account_update_params

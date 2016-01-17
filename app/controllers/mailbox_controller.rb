d14329ec68ee9cefb30198bfd477ffe55aa6322e
Mashpy Says <likewarid@gmail.com>
2016-01-17 12:03:52 +0000

    now message system working fine

--- app/controllers/mailbox_controller.rb
+++ app/controllers/mailbox_controller.rb
@@ -4,6 +4,9 @@ class MailboxController < ApplicationController
   def inbox
     @inbox = mailbox.inbox
     @active = :inbox
+    @receipts = conversation.receipts_for(current_user).order("created_at ASC")
+    # mark conversation as read
+    conversation.mark_as_read(current_user)
   end
 
   def sent

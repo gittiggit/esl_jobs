0b871c636fd0cfd85afe793a5a717dc401ab99c4
Mashpy Says <likewarid@gmail.com>
2016-01-17 19:12:44 +0000

    added company employee by username

--- app/controllers/company_employees_controller.rb
+++ app/controllers/company_employees_controller.rb
@@ -1,9 +1,27 @@
 class CompanyEmployeesController < InheritedResources::Base
+  
+  def create
+  @employees_new_id  = User.find_by(:username => params[:employee_name])
+  
+  if @employees_new_id.blank?
+    @employees_new_id  = User.find_by(:email => params[:employee_name])
+  end
+  
+  @company_employee = CompanyEmployee.new(company_employee_params)
+ 
+  if @company_employee.save
+  redirect_to user_path(current_user)
+    else
+      render 'new'
+    end
+  end
 
   private
 
     def company_employee_params
-      params.require(:company_employee).permit(:user_id, :employee_id)
+      unless @employees_new_id.blank?
+      params.require(:company_employee).permit(:user_id, :employee_id, :employee_name).merge(:employee_id => @employees_new_id.id, :user_id => current_user.id )
+      end
     end
 end
 
--- app/models/company_employee.rb
+++ app/models/company_employee.rb
@@ -1,2 +1,4 @@
 class CompanyEmployee < ActiveRecord::Base
+    validates :user_id, :presence => true
+    validates :employee_id, :presence => true
 end

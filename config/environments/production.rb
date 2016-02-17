1bf31c8aa411efcba1d52833b4860591ab23a56f
Mashpy Says <likewarid@gmail.com>
2016-02-17 21:03:35 +0000

    Update mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,13 @@ EslJobs::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.action_mailer.default_url_options = {:host => 'esljobslounge.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "127.0.0.1",
+    :port    => 25,
+    :domain  => 'esljobslounge.com'
+  }
+  
 end

d10a6192ca0333834d96723442c055f80b1513fa
Mashpy Says <likewarid@gmail.com>
2016-03-03 18:33:31 +0000

    From 1 to 3 and 8 to 10 jobs are featured

--- app/controllers/job_search_results_controller.rb
+++ app/controllers/job_search_results_controller.rb
@@ -1,6 +1,6 @@
 class JobSearchResultsController < ApplicationController
   def index
-    @job_posts = JobPost.advancesearch(params[:keyword],params[:countrysearch], params[:datesearch]).paginate(:page => params[:page], :per_page => 5).order('id DESC')
+    @job_posts = JobPost.advancesearch(params[:keyword],params[:countrysearch], params[:datesearch]).paginate(:page => params[:page], :per_page => 10).order('id DESC')
     @countries = Country.all
   end
 end
